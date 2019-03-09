e1c5b86fc78948bef28e994b9f46ae4f58674b1d
klimashevsky-pavel <pavel.shrw@gmail.com>
2019-03-09 17:29:43 +0300

    task-6 - moved db interactions to separate module

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,27 +1,27 @@
 import { Response } from 'express';
 import { ExtendedRequest } from 'interfaces/ExtendedRequest';
-import { db } from 'db';
+import { getAllProductsFromDb, getProductByIdFromDb, addProductToDb } from 'db/interactionsWithDB';
 
 const getAllProducts = (req: ExtendedRequest, res: Response) => {
-    db.Product.findAll().then(products => {
+    getAllProductsFromDb().then(products => {
         res.status(200).json({ products });
     });
 };
 
 const getSingleProduct = (req: ExtendedRequest, res: Response) => {
-    db.Product.findById(req.params.id).then(product => {
+    getProductByIdFromDb(req.params.id).then(product => {
         res.status(200).json({ product });
     });
 };
 
 const getSingleProductReviews = (req: ExtendedRequest, res: Response) => {
-    db.Product.findById(req.params.id).then(product => {
+    getProductByIdFromDb(req.params.id).then(product => {
         res.status(200).json({ reviews: product.reviews });
     });
 };
 
 const addProduct = (req: ExtendedRequest, res: Response) => {
-    db.Product.create(req.body)
+    addProductToDb(req.body)
         .then(result => {
             res.send(`New Product Successfully added: ${result}`);
         })
--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,9 +1,9 @@
 import { Response } from 'express';
 import { ExtendedRequest } from 'interfaces/ExtendedRequest';
-import { db } from 'db';
+import { getAllUsersFromDb } from 'db/interactionsWithDB';
 
 const getAllUsers = (req: ExtendedRequest, res: Response) => {
-    db.User.findAll().then(users => {
+    getAllUsersFromDb().then(users => {
         res.status(200).json({ users });
     });
 };
